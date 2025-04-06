775d273444b24501f1ff0343a8a214343fa818b2
pavansundar9 <pendempavansundar8955@gmail.com>
2025-04-06 11:13:44 +0530

    Added WebConfig.java

---
+++ springapp/src/main/java/com/example/springapp/configuration/WebConfig.java
@@ -0,0 +1,16 @@
+package com.example.springapp.configuration;
+
+// filepath: c:\Users\pende\VSCODE\ipl\springapp\src\main\java\com\example\springapp\configuration\WebConfig.java
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setPatternParser(null); // Use AntPathMatcher
+    }
+}
